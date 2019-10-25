ca554857c48fa519b282828c0eec6462e13b0cab
Santiago Gonzalez <sgonzalezhernandez081@gmail.com>
2019-10-25 13:00:32 -0500

    React Hooks: useEffect y useState

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,39 +7,54 @@ import CarouselItems from '../components/CarouselItems';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header></Header>
-        <Search></Search>
+const App = () => {
+    //state
+    const [ videos, setVideos ] = useState([]);
 
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItems />
-                <CarouselItems />
-                <CarouselItems />
-                <CarouselItems />
-                <CarouselItems />
-                <CarouselItems />
-            </Carousel>
-        </Categories>
+    //consumir API
+    useEffect( () => {
+        fetch('http://localhost:3000/initalState')
+            .then(response => response.json())
+            .then( data => setVideos(data));
+    },[]);
+    //^ escuchar propiedad que pueda cambiar
+
+    console.log(videos);
+
+    return( 
+        <div className="App">
+            <Header></Header>
+            <Search></Search>
+
+            <Categories title="Mi Lista">
+                <Carousel>
+                    <CarouselItems />
+                    <CarouselItems />
+                    <CarouselItems />
+                    <CarouselItems />
+                    <CarouselItems />
+                    <CarouselItems />
+                </Carousel>
+            </Categories>
 
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItems />
-                <CarouselItems />
-            </Carousel>
-        </Categories>
+            <Categories title="Tendencias">
+                <Carousel>
+                    <CarouselItems />
+                    <CarouselItems />
+                </Carousel>
+            </Categories>
 
 
-        <Categories title="Originales de Plazi Video">
-            <Carousel>
-                <CarouselItems />
-            </Carousel>
-        </Categories>
+            <Categories title="Originales de Plazi Video">
+                <Carousel>
+                    <CarouselItems />
+                </Carousel>
+            </Categories>
 
-        <Footer />
-    </div>
-);
+            <Footer />
+        </div>
+    );
+}
 
 export default App
