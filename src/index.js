41827bc1ef91efd7fbd69ceb4f35892da647ee77
Santiago Gonzalez <sgonzalezhernandez081@gmail.com>
2019-10-31 12:28:25 -0500

    install redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    //inicialisar
+    <Provider>
+      <App />
+    </Provider>,
+
+    document.getElementById('app')
+);
